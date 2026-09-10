5b5749bfb67b26fc501ca224239ce58493b7ad1e
Andres Gutierrez <andresfelipegutierrez04@gmail.com>
2026-09-10 10:44:11 -0500

    se Simplfico el app.jsx, se creo Hook UseConcto y contactoService para mejor manejo de errores

--- src/Services/contactosServices.js
+++ src/Services/contactosServices.js
@@ -21,3 +21,131 @@ export async function obtenerContactos(){
 
 
 }
+
+
+export async function  crearContacto(nuevoContacto) {
+        
+    // Validaciones
+
+    if (!nuevoContacto.nombre.trim()) {
+      alert("El nombre es obligatorio");
+      return;
+    }
+
+    if (!nuevoContacto.telefono.trim()) {
+      alert("El teléfono es obligatorio");
+      return;
+    }
+
+    if (!nuevoContacto.correo.includes("@")) {
+      alert("Correo inválido");
+      return;
+    }
+
+
+    try{
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(nuevoContacto)
+        }
+    )
+
+        if(!response.ok){
+            throw new Error("No se pudo agregar")
+        }
+
+        const data = await response.json()
+        
+        return data
+        
+    }
+    catch(error){
+        console.log(error);
+        alert("Error al guardar contacto")
+
+    }
+        
+    
+
+
+
+
+
+    }
+
+
+
+export async function borrarContacto(id) {
+    
+    if(!id){
+        return alert("No existe este ID")
+    }
+    
+    try{
+        const response = await fetch(`${API_URL}/${id}`, {
+            method : "DELETE"
+        })
+
+        if(!response.ok){
+            throw new Error("No se pudo Eliminar al contacto")
+        }
+
+        return response
+
+
+    }catch(error){
+        console.log("Error al eliminar contacto");
+           
+    }
+
+}
+
+
+export async function actualizarContacto(id, contactoActualizado) {
+    
+    try {
+        if(!id){
+            alert("No existe contacto")
+            return 
+        }
+        if (!contactoActualizado.nombre.trim()) {
+            alert("El nombre es obligatorio");
+            return
+        }
+        if (!contactoActualizado.correo.includes("@")) {
+            alert("Correo inválido");
+            return;
+    }
+        if (!contactoActualizado.telefono.trim()) {
+            alert("El Teefono es obligatorio");
+            return 
+        }
+        if (!contactoActualizado.etiqueta.trim()) {
+            alert("La etiqueta es obligatorio");
+            return 
+        }
+
+        const responde = await fetch(`${API_URL}/${id}`, {
+            method : "PATCH",
+            headers: {
+                "Content-Type" : "application/json" 
+            },
+            body: JSON.stringify(contactoActualizado)
+        })
+
+        if(!responde.ok){
+            throw new Error("No se pudo actualizar el Contacto")
+        }
+
+        const data = await responde.json()
+
+        return data
+
+    } catch (error) {
+        console.log("Error al actualizar contacto", error);
+        alert("Error al actualizar contacto")
+    }
+}
--- src/hooks/UseContactos.js
+++ src/hooks/UseContactos.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
-import { obtenerContactos } from "../Services/contactosServices"
+import { actualizarContacto, borrarContacto, crearContacto, obtenerContactos } from "../Services/contactosServices"
 
 
 function UseContactos(){
 
-    const [contactos2, setContactos] = useState([])
-
+    const [contactos, setContactos] = useState([])
+    const [contactoEditar, setContactoEditar] = useState(null);
+    
 
 
 
@@ -42,14 +43,101 @@ function UseContactos(){
 
     }, [])
 
-    
+
+    async function agregarContacto(nuevoContacto) {
+        
+        try {
+                const existe = contactos.some((c) =>
+                    c.correo === nuevoContacto.correo 
+                )
+
+
+            if(existe){
+                return alert("Ya existe un contacto con este Correo")
+            }
+            
+            const data = await crearContacto(nuevoContacto)
+
+            setContactos((prev) =>
+            [...prev, data]
+            )
+
+        } catch (error) {
+            console.error("error", error);
+            
+        }
+
+    }
+
+
+    async function eliminarContacto(id) {
+        try {
+            const existe = contactos.find(c =>
+                c.id === id
+            )
+            
+            if(!existe){
+                return alert("contacto no existe")
+            }
+            
+            const response = await borrarContacto(id)
+                        
+            if(response.ok){
+                alert("Se borro correctamente el contacto")
+            }
+
+            setContactos((prev) =>{
+                return prev.filter((p) => {
+                    return p.id !== id
+                })
+            })
+
+        } catch (error) {
+            console.log("No se pudo eliminar al contacto");
+            
+        }
+    }
+
+    async function editarContacto(id, contactoActualizado) {
+        
+        if(!contactoActualizado) return alert("No hay datos para actualizar")
+     
+        
+        try{
+            const data = await actualizarContacto(id,contactoActualizado)
+
+            setContactos((prev) => {
+                return prev.map((contacto) =>{
+                    return contacto.id === id
+                    ? data
+                    : contacto
+                })
+            })
+
+            setContactoEditar(null)
+
+        }catch(error){
+            console.log("error: ",error);
+            
+        }
+    }
     
 
     return{
-        contactos2
+        contactos,
+        agregarContacto,
+        eliminarContacto,
+        editarContacto,
+        setContactoEditar,
+        contactoEditar
     }
+
+
+
+
 }
 
 export default UseContactos
+
 
 
